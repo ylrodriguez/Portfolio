106dba3911cc61d8a4d9708fe512370d6ce4ab22
Yojhan <ylrodriguez024@gmail.com>
2019-08-21 12:50:20 -0500

    Add @Keyframe navSlideDown to .fixed class in CSS, to make a smooth transition in the nav element.

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-    loadEventHandlers()
+const ABOUTMESECTION = document.querySelector('#about-me');
+const HOMESECTION = document.querySelector('#home');
+const BUTTONSEEMORE = document.querySelector('.see-more');
+const NAVELEMENT = document.getElementsByTagName("nav")[0];
+var navElementHasFixedClass = false;
+
+
+document.getElementsByClassName("fa-bars")[0].addEventListener("click", () => {
+    document.getElementsByClassName("wrap-link")[0].classList.toggle("visible")
 });
 
-function loadEventHandlers(){
-    document.getElementsByClassName("fa-bars")[0].addEventListener("click", () => {
-        document.getElementsByClassName("wrap-link")[0].classList.toggle("visible")
+BUTTONSEEMORE.addEventListener("click", () => {
+    ABOUTMESECTION.scrollIntoView({
+        behavior: "smooth"
     });
+    ABOUTMESECTION.focus();
+})
+
+window.onscroll = (e) => {
+    // Se recoge la posición top del botón y
+     //se le suma la altura total del boton y 100 de margen adicional
+    //var contentHeight = BUTTONSEEMORE.offsetTop + BUTTONSEEMORE.offsetHeight + 100;
+    var contentHeight = HOMESECTION.offsetHeight;
+    var yOffSet = window.pageYOffset;
+
+    if(yOffSet >= contentHeight && !navElementHasFixedClass){
+        NAVELEMENT.classList.add("fixed");
+        navElementHasFixedClass = true;
+    }
+    else if(yOffSet < (contentHeight-60) && navElementHasFixedClass){
+        NAVELEMENT.classList.remove("fixed");
+        navElementHasFixedClass = false;
+    }
+
 }
