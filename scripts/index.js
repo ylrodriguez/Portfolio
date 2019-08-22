25cad5df65b6124d1cfd217318c79efcaffeac51
Yojhan <ylrodriguez024@gmail.com>
2019-08-22 11:35:43 -0500

    Add animations to the title and the hexagon of the AboutMe section

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,14 @@
-const ABOUTMESECTION = document.querySelector('#about-me');
 const HOMESECTION = document.querySelector('#home');
-const BUTTONSEEMORE = document.querySelector('.see-more');
+const BUTTONSEEMORE = document.querySelector('#see-more');
 const NAVELEMENT = document.getElementsByTagName("nav")[0];
+
+const ABOUTMESECTION = document.querySelector('#about-me');
+
 const WRAPLINKELEMENT = document.getElementsByClassName("wrap-link")[0];
+
 var navElementHasFixedClass = false;
+var aboutMeTitleAnimated = false;
+var aboutMeHexagonAnimated = false;
 
 
 document.getElementsByClassName("fa-bars")[0].addEventListener("click", () => {
@@ -19,12 +24,24 @@ BUTTONSEEMORE.addEventListener("click", () => {
 
 window.onscroll = (e) => {
     WRAPLINKELEMENT.classList.remove("visible");
-    // Se recoge la posición top del botón y
-     //se le suma la altura total del boton y 100 de margen adicional
-    //var contentHeight = BUTTONSEEMORE.offsetTop + BUTTONSEEMORE.offsetHeight + 100;
-    var contentHeight = HOMESECTION.offsetHeight;
     var yOffSet = window.pageYOffset;
 
+    addOrRemoveFixedNav(yOffSet, HOMESECTION.offsetHeight);
+
+    if(!aboutMeTitleAnimated){
+        if((BUTTONSEEMORE.offsetTop + BUTTONSEEMORE.offsetHeight) <= yOffSet){
+            animateTitleOfASection("#about-me");
+        }
+    }
+
+    if(!aboutMeHexagonAnimated){
+        if((BUTTONSEEMORE.offsetTop + BUTTONSEEMORE.offsetHeight + 100) <= yOffSet){
+            animateAboutMeElementHexagon();
+        }
+    }
+}
+
+function addOrRemoveFixedNav(yOffSet, contentHeight){
     if(yOffSet >= contentHeight && !navElementHasFixedClass){
         NAVELEMENT.classList.add("fixed");
         navElementHasFixedClass = true;
@@ -33,5 +50,26 @@ window.onscroll = (e) => {
         NAVELEMENT.classList.remove("fixed");
         navElementHasFixedClass = false;
     }
-
 }
+
+function animateTitleOfASection(section){
+    var title = document.querySelector(section+" .title");
+    var titleBar = document.querySelector(section+" .title-bar");
+
+    title.classList.add("slide-to-left");
+    titleBar.style = "animation-delay: 0.5s"
+    titleBar.classList.add("slide-to-left");
+
+    aboutMeTitleAnimated = true;
+}
+
+function animateAboutMeElementHexagon(){
+    var elementHexagon = document.querySelector("#about-me .element.hex");
+    elementHexagon.classList.add("show");
+    aboutMeHexagonAnimated = true;
+}
+
+document.querySelector('#cv').addEventListener("click", () => {
+    var urlCV = "https://1drv.ms/b/s!AlaKRn08I0M_gvgnoeXaLTesRnOzKQ?e=Dh5LhX";
+    window.open(urlCV,"_blank ");
+});
